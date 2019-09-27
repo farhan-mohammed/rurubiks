66f5ba748aa29c770fc6c454bea5ceac103718d5
Farhan Mohammed <farhanhm12@gmail.com>
2019-09-27 17:39:14 -0400

    Updates

--- js/carousel.js
+++ js/carousel.js
@@ -5,18 +5,13 @@ const carouselSize = document.getElementsByClassName('hnav-item').length;
 
 $(`.s1`).click(()=>{
     $carousel.attr('data-slide',"1");
-    console.log($carousel.width())
-    console.log(`1 was clicked`)
 })
 $(`.s2`).click(()=>{
     $carousel.attr('data-slide',"2");
-    console.log(`2 was clicked`)
 })
 $(`.s3`).click(()=>{
     $carousel.attr('data-slide',"3");
-    console.log(`3 was clicked`)
 })
 $(`.s4`).click(()=>{
     $carousel.attr('data-slide',"4");
-    console.log(`4 was clicked`)
 })
